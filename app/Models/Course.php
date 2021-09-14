44bb9b5bdcc3f7a3facd8e6f3338abdeb2cdccdd
brian mureithi <brianmurithi65@gmail.com>
2021-09-14 10:35:37 +0300

    initial upload of files

---
+++ app/Models/Course.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Course extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        "name",
+        "description",
+        "amount"
+    ];
+    public function payment(){
+        return $this->hasMany(payments::class, 'course_id','id');
+    }
+    public function videos(){
+        return $this->hasMany(Video::class, 'course_id','id');
+    }
+
+    public function users(){
+        return $this->hasMany(User::class, 'course_id','id');
+    }
+
+}
---
+++ database/migrations/2021_09_10_055136_create_videos_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateVideosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('videos', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->unsignedBigInteger('course_id');
+            $table->foreign('course_id')->references('id')->on('courses')->unsigned()->index();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('videos');
+    }
+}
