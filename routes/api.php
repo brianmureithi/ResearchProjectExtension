3b42d5b3abbe92e8b3fadf9d9c743d4a6291bbd7
brianmureithi <brianmurithi65@gmail.com>
2022-01-03 12:32:28 +0300

    almost

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-/* Route::post('/stkpush',[App\Http\Controllers\MpesaController::class, 'stkPush'])->name('stkpush.post'); */
+Route::post('/stkpush',[App\Http\Controllers\MpesaController::class, 'stkPush'])->name('stkpush.post');
